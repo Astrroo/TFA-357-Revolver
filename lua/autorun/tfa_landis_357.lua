3cd8fb9290484b80a00312b089c63ce07ce4ce7d
Astrroo <95949320+Astrroo@users.noreply.github.com>
2021-12-10 23:27:06 -0600

    Update tfa_landis_357.lua
    Filled out

--- lua/autorun/tfa_landis_357.lua
+++ lua/autorun/tfa_landis_357.lua
@@ -1,35 +1,35 @@
 sound.Add(
 {
-    name = "",
+    name = "Weapon_357.OpenLoader",
     channel = CHAN_ITEM,
     volume = 1.0
     soundlevel = 80,
-    sound = "",
+    sound = "weapons/357/357_reload1.wav",
 })
 
 sound.Add(
 {
-    name = "",
+    name = "Weapon_357.RemoveLoader",
     channel = CHAN_ITEM,
     volume = 1.0
     soundlevel = 80,
-    sound = "",
+    sound = "weapons/357/357_reload3.wav",
 })
 
 sound.Add(
 {
-    name = "",
+    name = "Weapon_357.ReplaceLoader",
     channel = CHAN_ITEM,
     volume = 1.0
     soundlevel = 80,
-    sound = "",
+    sound = "weapons/357/357_reload4.wav",
 })
 
 sound.Add(
 {
-    name = "",
+    name = "Weapon_357.Spin",
     channel = CHAN_ITEM,
     volume = 1.0
     soundlevel = 80,
-    sound = "",
+    sound = "weapons/357/357_spin1.wav",
 })
