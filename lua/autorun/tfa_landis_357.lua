f12b9f2f0124720901830367512ed27885f13a15
Astrroo <95949320+Astrroo@users.noreply.github.com>
2021-12-10 23:23:15 -0600

    Update tfa_landis_357.lua
    laid out

--- lua/autorun/tfa_landis_357.lua
+++ lua/autorun/tfa_landis_357.lua
@@ -1,1 +1,35 @@
+sound.Add(
+{
+    name = "",
+    channel = CHAN_ITEM,
+    volume = 1.0
+    soundlevel = 80,
+    sound = "",
+})
 
+sound.Add(
+{
+    name = "",
+    channel = CHAN_ITEM,
+    volume = 1.0
+    soundlevel = 80,
+    sound = "",
+})
+
+sound.Add(
+{
+    name = "",
+    channel = CHAN_ITEM,
+    volume = 1.0
+    soundlevel = 80,
+    sound = "",
+})
+
+sound.Add(
+{
+    name = "",
+    channel = CHAN_ITEM,
+    volume = 1.0
+    soundlevel = 80,
+    sound = "",
+})
